0756c40d1d112d62e1d05c10c61150ca5668d0d0
Sanchit <sanchittewari222@gmail.com>
2023-07-03 20:26:47 +0530

    increased limit

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ mongoose.connect(URI).then(() => {
 // MIDDLEWARES 
 app.use(cors());
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json({limit:'50mb'}));
 app.use(fileUpload({
     useTempFiles: true
 }))
